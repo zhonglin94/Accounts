fe84f2f0ba080a611288e9e09f98a705c940d067
Zhonglin Yang <ZYang25@slb.com>
2019-06-30 11:10:21 +0800

    Add a unit test for the IsSameMonthOfSameYear extension

---
+++ AccountsLibraryTest/AccountsLibraryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountsLibrary.Extensions;
+
+namespace AccountsLibraryTest
+{
+    [TestClass]
+    public class AccountsLibraryTest
+    {
+        [TestMethod]
+        public void should_return_true_when_the_two_dateTime_is_the_same_year_same_month()
+        {
+            DateTime dateTime = new DateTime(2019, 6, 30);
+            DateTime targetDateTime = new DateTime(2019, 6, 30);
+            bool result = dateTime.IsSameMonthOfSameYear(targetDateTime);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ AccountsLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccountsLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccountsLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b6719c9-4757-4597-8434-927b7c78205d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
